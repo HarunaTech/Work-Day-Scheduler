b6c49604dfad6b4b04b063f665a993f575da3b6c
ohwolarby <122782321+ohwolarby@users.noreply.github.com>
2024-01-23 00:33:09 +0200

    Added clickable save button

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
+// select html with class save button, gave it event listen when clicked,(jQuery event listener)
+$(".saveBtn").on("click",function(){
+
+    console.log("You clicked save button"); // testing to see if the save button clicked and worked from the console 
 
 
 
-
+})
